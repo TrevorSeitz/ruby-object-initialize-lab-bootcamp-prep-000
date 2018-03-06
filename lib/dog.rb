e5752dfdbe168745dc2aa9ea876ecbcab9aa4eaf
Trevor Seitz <trevinla@gmail.com>
2018-03-06 22:01:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,16 @@ class Dog
   def name
     @name
   end
+
+  def initialize(breed)
+    @breed = name
+  end
+
+  def name=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
